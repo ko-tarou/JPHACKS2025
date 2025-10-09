55a72422f87f56c263e921679e24a7ee4bd54b9d
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-10-09 15:17:07 +0900

    ボタンでスクロール制御

--- JPHACKS2025/Views/RecipeView.swift
+++ JPHACKS2025/Views/RecipeView.swift
@@ -1,38 +1,91 @@
 import SwiftUI
 
 struct RecipeView: View {
+    // 📌 1. スクロール先のIDを保持する状態変数
+    @State private var currentScrollID = 0
+    // 項目の総数
+        private let itemCount = 10
+    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                Color.clear
-                
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, recipe")
-                
-                Text("料理名")
-                    .font(.title)
-                
-                ForEach(0..<10, id: \.self) { num in
-                    Text("\(num)")
-                        .font(.title)
-                    Text("""
-                        hogehoge
-                        hogehoge
-                        hogehoge
-                        hogehoge
-                        """)
-                        .padding(10)
-                        .font(.title)
+        ScrollViewReader { proxy in
+            ZStack {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Color.clear
+                        
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundStyle(.tint)
+                        Text("Hello, recipe")
+                        
+                        Text("料理名")
+                            .font(.title)
+                        
+                        ForEach(0..<itemCount, id: \.self) { num in
+                            
+                            Text("\(num)")
+                                .font(.title)
+                                .id(num)
+                            
+                            Text("""
+                                hogehoge
+                                hogehoge
+                                hogehoge
+                                hogehoge
+                                """)
+                                .padding(10)
+                                .font(.title)
+                        }
+                    }
+                    .padding()
+                    .frame(width: .infinity)
                 }
+                
+                
+                HStack{
+                    Spacer()
+                    
+                    VStack{
+                        // 上ボタン
+                        Button {
+                            guard 0 < currentScrollID else { return }
+                            currentScrollID -= 1
+                            
+                            withAnimation { // スムーズにスクロールさせる
+                                proxy.scrollTo(currentScrollID, anchor: .top)
+                            }
+                        } label: {
+                            Image(systemName: "arrowshape.up.fill")
+                                .padding()
+                        }
+                        .font(.system(size: 60))
+                        .buttonStyle(.borderedProminent)
+                        
+                        // 下ボタン
+                        Button {
+                            guard currentScrollID < itemCount-3 else { return }
+                            currentScrollID += 1
+                            
+                            withAnimation { // スムーズにスクロールさせる
+                                proxy.scrollTo(currentScrollID, anchor: .top)
+                            }
+                        } label: {
+                            Image(systemName: "arrowshape.down.fill")
+                                .padding()
+                        }
+                        .font(.system(size: 60))
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+
             }
-            .padding()
-            .frame(width: .infinity)
         }
     }
 }
 
+
 #Preview {
     RecipeView()
 }
+
+
